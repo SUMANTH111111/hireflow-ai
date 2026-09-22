cbd4a5caf9d85fd1c2f56b6afd9b12757a66b123
Poruri Nageswara Sumanth <192411082.simats@saveetha.com>
2026-09-22 09:13:02 +0530

    Meaningful feature message

--- app.py
+++ app.py
@@ -1,15 +1,54 @@
-from flask import Flask
-from config import Config
-from models.db import mysql
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-mysql.init_app(app)
+# ---------- MySQL ----------
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "Root@991241"
+app.config["MYSQL_DB"] = "hireflow_ai"
 
+mysql = MySQL(app)
+
+# ---------- Login ----------
 @app.route("/")
 def home():
-    return "HireFlow AI is Running 🚀"
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    email = request.form["email"]
+    password = request.form["password"]
+    role = request.form["role"]
+
+    cur = mysql.connection.cursor()
+
+    cur.execute(
+        "SELECT * FROM users WHERE email=%s AND password=%s AND role=%s",
+        (email, password, role)
+    )
+
+    user = cur.fetchone()
+    cur.close()
+
+    if user:
+        if role == "enterprise":
+            return redirect("/enterprise")
+
+        return redirect("/recruiter")
+
+    return "Invalid credentials"
+
+# ---------- Dashboards ----------
+@app.route("/enterprise")
+def enterprise():
+    return render_template("enterprise_dashboard.html")
+
+@app.route("/recruiter")
+def recruiter():
+    return render_template("recruiter_dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
-    MYSQL_PASSWORD = "YOUR_PASSWORD"
+    MYSQL_PASSWORD = "Root@991241"
     MYSQL_DB = "hireflow_ai"
 
     SECRET_KEY = "hireflow_ai_secret_key"
